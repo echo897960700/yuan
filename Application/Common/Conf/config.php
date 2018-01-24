ffb3c82a9d271d35e3a59b2ad8c8fa4347445469
echo <897960700@qq.com>
2018-01-24 12:02:54 +0800

    1

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,8 +5,8 @@ return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_NAME'   => 'pra', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '', // 密码
+	'DB_USER'   => 'caijunpeng', // 用户名
+	'DB_PWD'    => 'Cjp123456.', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'p_',// 数据库表前缀 
 	'DB_CHARSET'=> 'utf8',// 字符集
